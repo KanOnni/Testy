b68c5fbcafb7bc502fd1607751ed5f72b89f7c68
Onni Kanerva <onni.kanerva@edu.salpaus.fi>
2024-09-04 11:54:10 +0300

    updated product schema

--- Backend/Models/products.js
+++ Backend/Models/products.js
@@ -2,10 +2,15 @@ const mongoose = require('mongoose');  // Import mongoose
 
 // Define the schema
 const productSchema = new mongoose.Schema({
-    title: { type: String, required: true },
+    name: { type: String, required: true },
+    type: {type: String, require: true },
+    description: { type: String, required: true },
     price: { type: Number, required: true },
     category: { type: String, required: true },
-    description: { type: String, required: true },
+    discount: { type: Number, required: false },
+    color: { type: String, required: true },
+    size: { type: String, required: true },
+    amount: { type: Number, required: true },
     image: { type: String, required: false }
 });
 
