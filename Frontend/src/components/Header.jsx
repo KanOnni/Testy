af8ac225b8b7ed1a492b806799c3b255729240a6
AleksiLuukkanen <aleksi.luukkanen@edu.salpaus.fi>
2024-10-08 13:07:24 +0300

    Css Fix 2

--- Frontend/src/components/Header.jsx
+++ Frontend/src/components/Header.jsx
@@ -19,29 +19,12 @@ const Header = () => {
         <header>
             <nav>
                 <img src={LogoSvg} alt="Testy logo" className="logo" />
-                <div className={`hamburger-menu ${isMenuOpen ? 'open' : ''}`} onClick={toggleMenu}>
-                    <div className="hamburger-icon">
-                        <span></span>
-                        <span></span>
-                        <span></span>
-                    </div>
-                </div>
                 <ul className={`nav-links ${isMenuOpen ? 'hidden' : ''}`}>
                     <li><Link to="/">Man</Link></li>
                     <li><Link to="/woman">Woman</Link></li>
                     <li><Link to="/children">Children</Link></li>
                 </ul>
-                <div className={`menu ${isMenuOpen ? 'open' : ''}`}>
-                    <div className="menu-header">
-                        <button className="close-button" onClick={toggleMenu}>X</button>
-                    </div>
-                    <ul>
-                        <li><a href="/clothes" onClick={() => handleMenuItemClick('Clothes')}>Clothes</a></li>
-                        <li><a href="/shoes" onClick={() => handleMenuItemClick('Shoes')}>Shoes</a></li>
-                        <li><a href="/accessories" onClick={() => handleMenuItemClick('Accessories')}>Accessories</a></li>
-                        <li><a href="/sale" onClick={() => handleMenuItemClick('Sale')}>Sale</a></li>
-                    </ul>
-                </div>
+            
             </nav>
         </header>
     );
